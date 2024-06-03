6e86cf46126f6a3647d9fbe4a0ae94726b64455a
changXXX <53431177+ChangXXX@users.noreply.github.com>
2024-06-03 22:09:42 +0900

    [ADD] EditTopDetail ui 초기 작업

--- feature/main/src/main/java/team/wbt/feature/main/edit/EditScreen.kt
+++ feature/main/src/main/java/team/wbt/feature/main/edit/EditScreen.kt
@@ -1,12 +1,34 @@
 package team.wbt.feature.main.edit
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import team.wbt.feature.main.edit.components.EditTopBar
 
 @Composable
@@ -23,6 +45,55 @@ fun EditScreen() {
             .imePadding()
             .navigationBarsPadding()
     ) { paddingValues ->
+        EditTopDetail(
+            modifier = Modifier.padding(paddingValues)
+        )
+        Spacer(modifier = Modifier.height(10.dp))
 
+    }
+}
+
+@Composable
+private fun EditTopDetail(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .background(Color.White)
+            .padding(16.dp),
+        horizontalAlignment = Alignment.Start,
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Text(
+            text = "라온 약국",
+            fontSize = 18.sp
+        )
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = "10000원",
+                fontWeight = FontWeight.Bold,
+                fontSize = 36.sp
+            )
+            Spacer(modifier = Modifier.padding(2.dp))
+            IconButton(
+                onClick = { /*TODO*/ },
+                modifier = Modifier.size(48.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Edit,
+                    modifier = Modifier.size(24.dp),
+                    contentDescription = null
+                )
+            }
+        }
+        Text(
+            text = "명세서 | 인식금액 10,000원",
+            textDecoration = TextDecoration.Underline,
+            color = Color.Gray.copy(alpha = 0.5f)
+        )
     }
 }
